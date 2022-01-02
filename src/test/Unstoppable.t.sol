2c7db467ab38e1382183202a7f4e6683b9f431d2
Camden <camdenaws@gmail.com>
2022-01-01 18:03:21 -0800

    reformat the unstoppable contract to use foundry

---
+++ src/test/Unstoppable.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "../unstoppable/UnstoppableLender.sol";
+import "../unstoppable/ReceiverUnstoppable.sol";
+import "../DamnValuableToken.sol";
+
+interface Vm {
+    function deal(address who, uint256 amount) external;
+    function expectRevert(bytes calldata) external;
+}
+
+contract ContractTest is DSTest {
+    UnstoppableLender unstoppableLender;
+    DamnValuableToken damnValuableToken;
+    ReceiverUnstoppable receiverUnstoppable;
+
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    function setUp() public {
+        damnValuableToken = new DamnValuableToken();
+        unstoppableLender = new UnstoppableLender(address(damnValuableToken));
+        receiverUnstoppable = new ReceiverUnstoppable(address(unstoppableLender));
+        
+        vm.deal(address(this), 1000000);
+
+        damnValuableToken.approve(address(unstoppableLender), 1000000);
+        unstoppableLender.depositTokens(1000000);
+        
+        damnValuableToken.transfer(address(this), 100);
+
+        receiverUnstoppable.executeFlashLoan(10);
+    }
+
+    function testExample() public {
+        vm.expectRevert("Transfer of tokens failed");
+        receiverUnstoppable.executeFlashLoan(10);
+    }
+}
